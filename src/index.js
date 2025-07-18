aa9288903aa5a52469c99a3ee2e642a56b7e84e8
Jiseung <jsg1504@gmail.com>
2025-07-19 02:12:54 +0900

    Feat: Generate HTML chart from JSON input

--- src/index.js
+++ src/index.js
@@ -1,15 +1,58 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+const { ChartJSNodeCanvas } = require('chartjs-node-canvas');
+const fs = require('fs');
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('greeting');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+async function run() {
+  try {
+    const chartDataInput = core.getInput('chart-data', { required: true });
+    const chartData = JSON.parse(chartDataInput);
+
+    const width = 800;
+    const height = 600;
+    const configuration = {
+      type: 'bar',
+      data: chartData,
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      },
+      plugins: [{
+        id: 'background-colour',
+        beforeDraw: (chart) => {
+          const ctx = chart.ctx;
+          ctx.save();
+          ctx.fillStyle = 'white';
+          ctx.fillRect(0, 0, width, height);
+          ctx.restore();
+        }
+      }]
+    };
+
+    const chartJSNodeCanvas = new ChartJSNodeCanvas({ width, height, backgroundColour: 'white' });
+    const imageBuffer = await chartJSNodeCanvas.renderToBuffer(configuration);
+    const imageBase64 = imageBuffer.toString('base64');
+
+    const htmlContent = `
+      <!DOCTYPE html>
+      <html>
+        <head>
+          <title>Chart Report</title>
+        </head>
+        <body>
+          <h1>Chart Report</h1>
+          <img src="data:image/png;base64,${imageBase64}" alt="Chart">
+        </body>
+      </html>
+    `;
+
+    core.setOutput('html-report', htmlContent);
+
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+run();
